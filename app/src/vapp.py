5f014814a86e7a0efd280f33dc90defc6ef3fd78
ringo <ringo.stahl@gmail.com>
2023-09-28 12:24:02 +0200

    initial implementation

--- app/src/vapp.py
+++ app/src/vapp.py
@@ -55,14 +55,40 @@ class SeatAdjusterApp(VehicleApp):
         """Run when the vehicle app starts"""
         # TODO subscribe to Vehicle.Cabin.Seat.Row1.Pos1.Position and provide
         # on_seat_position_changed as callback.
-        pass
+        await self.Vehicle.Cabin.Seat.Row1.Pos1.Position.subscribe(
+            self.on_seat_position_changed)
+        
 
     async def on_seat_position_changed(self, data: DataPointReply):
         # TODO publish the current position as MQTT message to CURRENT_POSITION_TOPIC.
-        pass
+        seat_position = data.get(
+            self.Vehicle.Cabin.Seat.Row1.Pos1.Position
+            ).value
+        
+        # publish position
+        await self.publish_event(
+            CURRENT_POSITION_TOPIC,
+            str(seat_position)
+        )
+
 
     @subscribe_topic(SET_POSITION_REQUEST_TOPIC)
     async def on_set_position_request_received(self, data_str: str) -> None:
         # TODO react on the position request and publish a MQTT message to
         # SET_POSITION_RESPONSE_TOPIC with the result of the action.
-        pass
+        logger.debug(
+            "Seat Position Request %s",
+            data_str
+        )
+
+        await self.Vehicle.Cabin.Seat.Row1.Pos1.Position.set(int(data_str))
+
+        seat_position = ( 
+            await
+            self.Vehicle.Cabin.Seat.Row1.Pos1.Position.get()
+            ).value
+
+        await self.publish_event(
+            SET_POSITION_RESPONSE_TOPIC,
+            str(seat_position)
+        )
